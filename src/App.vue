baf5ab3306f86b74f53f697ab2c0b0dd1186d661
Imer <imer1319nd@gmail.com>
2023-12-05 11:01:56 -0400

    cambios al bg

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <script setup>
-import InfoUser from './components/InfoUser.vue';
-import Menu from './components/Menu.vue';
-import Navbar from './components/Navbar.vue';
+import InfoUser from "./components/InfoUser.vue";
+import Menu from "./components/Menu.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-  <div id="inicio"
-    class="bg-homeBg  min-h-screen bg-no-repeat bg-center bg-cover bg-fixed pb-12 md:pb-8 w-full">
+  <div
+    id="inicio"
+    class="bg-homeBg min-h-screen bg-no-repeat bg-center bg-cover bg-fixed pb-12 md:pb-8 w-full"
+  >
     <Navbar />
-    <div class="container grid grid-cols-12 md:gap-10 justify-between lg:mt-[120px]">
+    <div
+      class="container grid grid-cols-12 md:gap-10 justify-between lg:mt-[120px]"
+    >
       <InfoUser />
       <div class="col-span-12 lg:col-span-8">
         <Menu></Menu>
-        <div class="rounded-2xl bg-white  p-6 lg:p-12 lg:pb-6" id="content">
-
+        <div class="rounded-2xl bg-white p-6 lg:p-12 lg:pb-6" id="content">
           <router-view v-slot="{ Component, route }">
             <transition name="fade" mode="out-in">
               <div :key="route.name">
@@ -21,11 +24,8 @@ import Navbar from './components/Navbar.vue';
               </div>
             </transition>
           </router-view>
-
         </div>
       </div>
     </div>
   </div>
 </template>
-
-<style scoped></style>
