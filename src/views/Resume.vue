9fa1758faeaef95d825267bd3c0201d42020f6da
Imer <imer1319nd@gmail.com>
2023-12-06 15:08:34 -0400

    pdf y modulo de proyectos

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,89 +1,82 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-    <h2 class="after-effect after:left-48">Resumen</h2>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-6 gap-y-6 mt-[30px]">
-        <!-- eductation start -->
-        <div class="col-span-2 md:col-span-1">
-            <div class="flex items-center space-x-2 mb-4">
-                <i class="fa-solid text-6xl text-[#6929E7] fa-graduation-cap"></i>
-                <h4 class="text-5xl font-medium"> Educación </h4>
-            </div>
-            <div
-                class="bg-[#fff4f4] py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg  ">
-                <span class="text-tiny text-gray-lite ">2015-2021</span>
-                <h3 class="text-xl "> Ingeniero informatico </h3>
-                <p class=""> U.A Juan Misael Saracho </p>
-            </div>
-            <div
-                class="bg-[#fff1fb] py-4 pl-5 pr-3 space-y-2 rounded-lg mb-6  ">
-                <span class="text-tiny text-gray-lite ">2014</span>
-                <h3 class="text-xl "> Bachillerato </h3>
-                <p class="">U.E. Heroes del chaco</p>
-            </div>
-        </div>
-
-        <div>
-            <div class="flex items-center space-x-2 mb-4">
-                <i class="fa-solid text-6xl text-[#6929E7] fa-briefcase"></i>
-                <h4 class="text-5xl font-medium"> Experiencia </h4>
-            </div>
-            <div
-                class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg bg-[#eef5fa]  ">
-                <span class="text-tiny text-gray-lite ">ago - oct 2021</span>
-                <h3 class="text-xl "> Desarrollo de aplicación para Alli Turismo </h3>
-                <p class=""> Contribución para el desarrollo del sitio de alli turismo
-                    <br>
-                    Realización de altas bajas y modificaciones para el sitio.
-                    <br>
-                    Manejo de plataformas de pago <i>(Mercado pago).</i>
-                    <br>
-                    Desarrollado con laravel y vueJs.
-                </p>
-            </div>
-            <div
-                class="bg-[#f2f4ff] py-4 pl-5 pr-3 space-y-2 rounded-lg mb-6  ">
-                <span class="text-tiny text-gray-lite ">oct 2022 - abr 2023</span>
-                <h3 class="text-xl "> Desarrollo de sistema para una clínica </h3>
-                <p class="">
-                    Manejo de roles y permisos.
-                    <br>
-                    Impresion de boleta de historial, receta, estudios y consulta del paciente.
-                    <br>
-                    Desarrollado con laravel y vue.js
-                </p>
-            </div>
-        </div>
-
-
-        <div class="col-span-2">
-            <div class="flex items-center space-x-2 mb-4">
-                <h4 class="text-5xl font-medium"> Cursos realizados </h4>
-            </div>
-            <div
-                class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg bg-[#eef5fa]  ">
-                <span class="text-tiny text-gray-lite ">nov 2022</span>
-                <h3 class="text-xl "> Entrenamiento Full-Stack con Laravel y Next.js </h3>
-            </div>
-            <div
-                class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg bg-[#eef5fa]  ">
-                <span class="text-tiny text-gray-lite ">ene 2022</span>
-                <h3 class="text-xl "> Desarrollo de una red social con pruebas automatizadas </h3>
-            </div>
-            <div
-                class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg bg-[#eef5fa]  ">
-                <span class="text-tiny text-gray-lite ">oct 2021</span>
-                <h3 class="text-xl "> Aplicación de chat con laravel </h3>
-            </div>
-            <div
-                class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg bg-[#eef5fa]  ">
-                <span class="text-tiny text-gray-lite ">sep 2021</span>
-                <h3 class="text-xl "> SQL consultas básicas a complejas </h3>
-            </div>
-        </div>
+  <h2 class="after-effect after:left-48">Resumen</h2>
+  <div
+    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-6 gap-y-6 mt-[30px]"
+  >
+    <!-- eductation start -->
+    <div class="col-span-2 md:col-span-1">
+      <div class="flex items-center space-x-2 mb-4">
+        <i class="fa-solid text-6xl text-[#6929E7] fa-graduation-cap"></i>
+        <h4 class="text-5xl font-medium">Educación</h4>
+      </div>
+      <div class="bg-[#fff4f4] py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg">
+        <span class="text-tiny text-gray-lite">Jul 2023</span>
+        <h3 class="text-xl">Curso ingles</h3>
+        <p class="">Instituto de idiomas ULI</p>
+      </div>
+      <div class="bg-[#fff4f4] py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg">
+        <span class="text-tiny text-gray-lite">Feb 2015 - Dic 2021</span>
+        <h3 class="text-xl">Ingeniero informatico</h3>
+        <p class="">U.A Juan Misael Saracho</p>
+      </div>
+      <div class="bg-[#fff1fb] py-4 pl-5 pr-3 space-y-2 rounded-lg mb-6">
+        <span class="text-tiny text-gray-lite">Dic 2014</span>
+        <h3 class="text-xl">Bachillerato</h3>
+        <p class="">U.E. Heroes del chaco</p>
+      </div>
     </div>
+
+    <div>
+      <div class="flex items-center space-x-2 mb-4">
+        <i class="fa-solid text-6xl text-[#6929E7] fa-briefcase"></i>
+        <h4 class="text-5xl font-medium">Experiencia</h4>
+      </div>
+      <div class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg bg-[#eef5fa]">
+        <span class="text-tiny text-gray-lite">May - Jul 2023</span>
+        <h3 class="text-xl">SISTEMA PARA LA ENSEÑANZA DEL LENGUAJE DE SEÑAS</h3>
+        <p class="">
+          Creé un sistema innovador para enseñar lenguaje de señas, con interfaz
+          intuitiva y funcional. Utilicé Vue.js y Electron.js para facilitar el
+          aprendizaje efectivo de este medio de comunicación.
+        </p>
+      </div>
+      <div class="bg-[#f2f4ff] py-4 pl-5 pr-3 space-y-2 rounded-lg mb-6">
+        <span class="text-tiny text-gray-lite">oct 2022 - abr 2023</span>
+        <h3 class="text-xl">DESARROLLO DE SISTEMA ALLI TURISMO</h3>
+        <p class="">
+          Contribuí el desarrollo en el sitio web para Alli Turismo, gestionando
+          cambios y sobresaliendo en plataformas de pago como Mercado Pago,
+          elaborando en Laravel y Vuejs
+        </p>
+      </div>
+    </div>
+
+    <div class="col-span-2">
+      <div class="flex items-center space-x-2 mb-4">
+        <h4 class="text-5xl font-medium">Cursos realizados</h4>
+      </div>
+      <div class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg bg-[#eef5fa]">
+        <span class="text-tiny text-gray-lite">Nov 2022</span>
+        <h3 class="text-xl">Entrenamiento Full-Stack con Laravel y Next.js</h3>
+      </div>
+      <div class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg bg-[#eef5fa]">
+        <span class="text-tiny text-gray-lite">Ene 2022</span>
+        <h3 class="text-xl">
+          Desarrollo de una red social con pruebas automatizadas
+        </h3>
+      </div>
+      <div class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg bg-[#eef5fa]">
+        <span class="text-tiny text-gray-lite">Oct 2021</span>
+        <h3 class="text-xl">Aplicación de chat con laravel</h3>
+      </div>
+      <div class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg bg-[#eef5fa]">
+        <span class="text-tiny text-gray-lite">Sep 2021</span>
+        <h3 class="text-xl">SQL consultas básicas a complejas</h3>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
