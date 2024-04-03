3ef778ee85b36de7eb18fd5602ad94004a2f757e
imer1319 <imer1319nd@gmail.com>
2024-04-02 21:02:47 -0300

    modificacion al infouser

--- src/components/InfoUser.vue
+++ src/components/InfoUser.vue
@@ -7,7 +7,7 @@
     >
       <!-- profile image -->
       <img
-        src="/images/imagen.jpg"
+        src="/images/imagen3.png"
         class="w-[240px] absolute left-[50%] transform -translate-x-[50%] h-[240px] drop-shadow-xl mx-auto rounded-[20px] -mt-[140px]"
         alt="about"
       />
